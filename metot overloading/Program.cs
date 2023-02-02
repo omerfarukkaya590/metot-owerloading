f30d4f7706d42481701df2e34207cdaaa85810fc
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2023-02-02 17:43:24 +0300

    Add files via upload

---
+++ metot overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿// mett overlading  out parametreler
+string sayi="444";
+int outsayi;
+bool sonuc=int.TryParse(sayi, out outsayi);
+if (sonuc)
+{
+    Console.WriteLine("başarlı");
+    Console.WriteLine("444");
+
+}
+else
+{
+    Console.WriteLine("başarısız");
+}
+Metotlar instance =new Metotlar();
+instance.Topla(3,1,sayi, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+// overloading aşırı yükleme
+int ifade= 555;
+instance.EkranaYazdır(ifade);
+instance.EkranaYazdır("ömer"," faruk");
+
+
+
+class Metotlar
+{
+    public void Topla(int a, int b,string c,out int toplam)
+    {
+        toplam =  a+b;
+    }
+public void EkranaYazdır(string veri)
+{
+     Console.WriteLine(veri);
+}
+public void EkranaYazdır(int veri)
+{
+     Console.WriteLine(veri);
+}
+public void EkranaYazdır(string veri1,string veri2)
+{
+     Console.WriteLine(veri1+veri2);
+}
+}
+
